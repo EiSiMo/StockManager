49086bb29366c9dcf2ce642c7229f26f1293798d
Moritz Eigenauer <moritz.eigenauer@gmail.com>
2024-07-19 18:14:02 +0200

    restructure

--- main.py
+++ main.py
@@ -1,71 +1,108 @@
-from flask import Flask, render_template, Response, request
-from gtts import gTTS
 import pygame
 import io
+import os.path
 import pyzbar.pyzbar
 import cv2
 import threading
 import re
 import time
-from todoist_api_python.api import TodoistAPI
 
-codes_todo = []
+from flask import Flask, render_template, Response, request
+from todoist_api_python.api import TodoistAPI
+from gtts import gTTS
+
+
+class ProductDatabase(dict):
+    def __init__(self, filename, *args, **kwargs):
+        super(ProductDatabase, self).__init__(*args, **kwargs)
+
+        self.filename = filename
+
+        db_validation_regex = re.compile(r"^$|^(.+: (\d+, )*\d+\n)+$")
+
+        if os.path.exists(filename):
+            with open(filename, "r", encoding="utf8") as file:
+                text = file.read()
+                if re.fullmatch(db_validation_regex, text):
+                    for line in text.splitlines(keepends=False):
+                        line = line.split(": ")
+                        name = line[0]
+                        codes = line[1].split(", ")
+                        self.__setitem__(name, codes, save=False)
+                    self._save()
+                else:
+                    raise Exception("Invalid database format")
+        else:
+            open(filename, "a").close()
+
+    def __setitem__(self, name, codes, save=True):
+        if isinstance(codes, str):
+            codes = [codes]
+
+        if name in self.keys():
+            codes.extend(self.__getitem__(name))
+
+        super(ProductDatabase, self).__setitem__(name, codes)
+
+        self._sort()
+        if save:
+            self._save()
+
+    def __delitem__(self, name_or_code):
+        if name_or_code in self.keys():
+            super(ProductDatabase, self).__delitem__(name_or_code)
+            self._save()
+        else:
+            for name, codes in self.items():
+                if name_or_code in codes:
+                    codes.remove(name_or_code)
+                    super(ProductDatabase, self).__setitem__(name, codes)
+                    self._save()
+
+    def __len__(self):
+        count = 0
+        for codes in self.values():
+            count += len(codes)
+        return count
+
+    def _save(self):
+        result = str()
+        for name, codes in self.items():
+            result += f"{name}: {', '.join(codes)}\n"
+
+        with open(self.filename, "w", encoding="utf8") as file:
+            file.write(result)
+
+    def _sort(self):
+        sorted_items = sorted(self.items(), key=lambda s: s[0].lower())
+        self.clear()
+        self.update(sorted_items)
+
+    def find(self, code):
+        for name, codes in self.items():
+            if code in codes:
+                return name
+        return None
 
 
 class StockManager:
     def __init__(self):
         self.todoist_api_key = "5e3904e04bede42252be34b19d88a401240d6dc0"
         self.todoist_project_id = "2336429901"
-        self.product_database_file = "product_database.txt"
-        self.initialize_pygame()
 
-    def initialize_pygame(self):
+        self.product_database = ProductDatabase("product_database.txt")
+        self.codes_todo = ["000000000"]
+
         pygame.mixer.init()
-
-    def load_product_database(self):
-        with open(self.product_database_file, "r", encoding="utf8") as file:
-            lines = file.read().strip().split('\n')
-            data_dict = {}
-
-            for line in lines:
-                if ':' in line:
-                    category, products_string = line.split(':')
-                    category = category.strip()
-                    products = [p.strip() for p in products_string.split(',')]
-                    data_dict[category] = products
-                else:
-                    products = [p.strip() for p in line.split(',')]
-                    if "Kein Produktname hinterlegt" not in data_dict:
-                        data_dict["Kein Produktname hinterlegt"] = []
-                    data_dict["Kein Produktname hinterlegt"].extend(products)
-        return data_dict
-
-    def order_database(self):
-        with open(self.product_database_file, "r", encoding="utf8") as file:
-            lines = file.readlines()
-            lines.sort()
-        with open(self.product_database_file, "w", encoding="utf8") as file:
-            file.writelines(lines)
 
     def say(self, text):
-        # Initialize pygame mixer
         pygame.mixer.init()
-
-        # Generate speech using gTTS
         tts = gTTS(text, lang="de", slow=False)
-
-        # Save the speech to an in-memory byte-stream
         audio_stream = io.BytesIO()
         tts.write_to_fp(audio_stream)
-
-        # Move the byte-stream position to the beginning
         audio_stream.seek(0)
-
-        # Load and play the audio from the byte-stream
         pygame.mixer.music.load(audio_stream, "mp3")
         pygame.mixer.music.play()
-
-        # Wait until the audio finishes playing
         while pygame.mixer.music.get_busy():
             pygame.time.Clock().tick(10)
 
@@ -84,7 +121,7 @@ class StockManager:
                 vid.release()
                 vid = cv2.VideoCapture(0)
 
-    def add_item_to_einkaufsliste(self, item_name):
+    def add_item_to_todoist(self, item_name):
         has_quantifier_regex = re.compile(r"^\d+x .+$")
         api = TodoistAPI(self.todoist_api_key)
         already = False
@@ -102,94 +139,74 @@ class StockManager:
         if not already:
             api.add_task(content=item_name, project_id=self.todoist_project_id)
 
-    def start(self):
-        self.order_database()
+    def run(self):
         self.say("Vorratsmanager gestartet.")
         while True:
             code = self.scan_barcode()
             self.say("Code erkannt.")
-            db = self.load_product_database()
 
-            product_name = None
-            for key, values in db.items():
-                if code in values:
-                    product_name = key
+            product_name = self.product_database.find(code)
 
             if product_name:
                 self.say(f"Ich schreibe {product_name} auf deine Einkaufsliste.")
-                self.add_item_to_einkaufsliste(product_name)
+                self.add_item_to_todoist(product_name)
             else:
-                codes_todo.append(code)
-                self.say("Unbekanntes Produkt. Bitte in der Datenbank ergänzen!")
+                if code not in self.codes_todo:
+                    self.codes_todo.append(code)
+                    self.say("Unbekanntes Produkt. Bitte in der Datenbank ergänzen!")
+                else:
+                    self.say("Bitte öffne die Webseite, um das Produkt zu indizieren.")
 
 
 class WebInterface:
-    def __init__(self):
+    def __init__(self, manager):
+        self.manager = manager
         self.app = Flask(__name__)
         self.setup_routes()
 
-    def add_to_database(self, code, name):
-        name = name.strip()
-        code = code.strip()
-        with open("product_database.txt", "r", encoding="utf8") as file:
-            lines = file.readlines()
-
-            found = False
-            for index, line in enumerate(lines):
-                line = line[:-1]
-                if line.split(":")[0] == name:
-                    found = True
-                    if code not in line.split(": ")[1].split(", "):
-                        line += f", {code}\n"
-                        lines[index] = line
-                    break
-
-            if not found:
-                lines.append(f"{name}: {code}\n")
-
-        with open("product_database.txt", "w", encoding="utf8") as file:
-            file.write("".join(sorted(lines)))
-
     def generate_entries(self):
-        with open("product_database.txt", "r", encoding="utf8") as file:
-            return "\n".join([f"<div class='item'>{entry.split(':')[0]}</div>" for entry in file.readlines()])
+        result = ""
+        for name in self.manager.product_database.keys():
+            result += f"<div class='item'>{name}</div>\n"
+        return result
 
     def setup_routes(self):
         @self.app.route('/')
         def home():
             time.sleep(0.1)
-            code = next(iter(codes_todo), None)
+            code = next(iter(self.manager.codes_todo), None)
             if not code:
                 code = "Aktuell keine zu indizierenden Produkte"
             return render_template("index.html", entries=self.generate_entries(), code=code)
 
-        @self.app.route('/new', methods=["POST"])
+        @self.app.route("/new", methods=["POST"])
         def new():
-            if request.json["item_code"] in codes_todo:
-                codes_todo.remove(request.json["item_code"])
+            name = request.json["name"]
+            code = request.json["code"]
+            if code in self.manager.codes_todo:
+                self.manager.codes_todo.remove(code)
 
-            if request.json["item_selected"] == "+ NEUER EINTRAG":
-                self.add_to_database(request.json["item_code"], request.json["item_name"])
-            else:
-                self.add_to_database(request.json["item_code"], request.json["item_selected"])
-            return Response("{}", status=200, mimetype='application/json')
+            self.manager.product_database[name] = code
 
-        @self.app.route('/discard', methods=["POST"])
+            return Response("{}", status=200, mimetype="application/json")
+
+        @self.app.route("/discard", methods=["POST"])
         def discard():
-            if request.json["item_code"] in codes_todo:
-                codes_todo.remove(request.json["item_code"])
-            return Response("{}", status=200, mimetype='application/json')
+            code = request.json["code"]
+            if code in self.manager.codes_todo:
+                self.manager.codes_todo.remove(code)
+            return Response("{}", status=200, mimetype="application/json")
 
     def run(self, host="0.0.0.0", port=80):
         self.app.run(host=host, port=port, debug=True, use_reloader=False)
 
 
 if __name__ == "__main__":
-    interface = WebInterface()
-    interface_thread = threading.Thread(target=interface.run)
-    interface_thread.start()
-
     manager = StockManager()
-    manager.start()
+    manager_thread = threading.Thread(target=manager.run)
+    manager_thread.start()
 
-    interface_thread.join()
+    interface = WebInterface(manager)
+    interface.run()
+
+    manager_thread.join()
--- static/javascript/script.js
+++ static/javascript/script.js
@@ -2,15 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const items = document.querySelectorAll('.item');
     const submitButton = document.getElementById('add_button');
     const discardButton = document.getElementById('discard_button');
-    const item_name = document.getElementById('item_name');
+    const itemName = document.getElementById('item_name');
     const code = document.getElementById('product_code').textContent;
 
-    if (code == "Aktuell keine zu indizierenden Produkte") {
-        item_name.disabled = true;
+    // Disable controls if no products are to be indexed
+    if (code === "Aktuell keine zu indizierenden Produkte") {
+        itemName.disabled = true;
         submitButton.disabled = true;
         discardButton.disabled = true;
-
-        item_name.style.opacity = '0.5';
+        itemName.style.opacity = '0.5';
         submitButton.style.opacity = '0.5';
         discardButton.style.opacity = '0.5';
     } else {
@@ -18,78 +18,65 @@ document.addEventListener('DOMContentLoaded', function() {
             item.addEventListener('click', function() {
                 items.forEach(i => i.classList.remove('selected'));
                 item.classList.add('selected');
-                if (item.textContent != "+ NEUER EINTRAG") {
-                    item_name.disabled = true;
-                    item_name.value = "";
-                    item_name.style.opacity = '0.5';
+                if (item.textContent !== "+ NEUER EINTRAG") {
+                    itemName.disabled = true;
+                    itemName.value = "";
+                    itemName.style.opacity = '0.5';
                 } else {
-                    item_name.disabled = false;
-                    item_name.style.opacity = '1.0';
-                };
+                    itemName.disabled = false;
+                    itemName.style.opacity = '1.0';
+                }
             });
         });
         items[0].classList.add("selected");
     }
 
-
+    // Handle submit button click
     submitButton.addEventListener('click', function() {
         const selected = document.querySelector('.item.selected');
-        console.log(selected.textContent)
-        if ((selected.textContent == "+ NEUER EINTRAG" && item_name.value) || selected.textContent != "+ NEUER EINTRAG") {
+        if ((selected.textContent === "+ NEUER EINTRAG" && itemName.value) || selected.textContent !== "+ NEUER EINTRAG") {
+            let selectedName = selected.textContent === "+ NEUER EINTRAG" ? "" : selected.textContent;
             const data = {
-                item_selected: selected.textContent,
-                item_name: item_name.value,
-                item_code: code
+                name: itemName.value + selectedName,
+                code: code
             };
 
             fetch('/new', {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data)
             })
             .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+            .then(data => console.log('Success:', data))
+            .catch(error => console.error('Error:', error));
+
+            location.reload(true);
         } else {
             alert("Bitte gib einen Produktnamen ein");
         }
-        location.reload(true);
     });
 
+    // Handle discard button click
     discardButton.addEventListener('click', function() {
-
-        const data = {
-            item_code: code
-        };
+        const data = { code: code };
 
         fetch('/discard', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
         })
         .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+        .then(data => console.log('Success:', data))
+        .catch(error => console.error('Error:', error));
 
         location.reload(true);
     });
 
-    item_name.addEventListener('input', function() {
-        const filter = item_name.value.toLowerCase();
+    // Filter items based on input
+    itemName.addEventListener('input', function() {
+        const filter = itemName.value.toLowerCase().trim();
         items.forEach(item => {
-            if (item.textContent.toLowerCase().includes(filter) || item.textContent == "+ NEUER EINTRAG") {
+            if (item.textContent.toLowerCase().includes(filter) || item.textContent === "+ NEUER EINTRAG") {
                 item.classList.remove('hidden');
             } else {
                 item.classList.add('hidden');
